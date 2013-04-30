da4a48fd2d5cb6012e85c0157193a1c106ef03d5
Pranay <agarwalpranaya@gmail.com>
2013-04-30 10:59:35 +0530

    inserting graph

--- build_graph.py
+++ build_graph.py
@@ -10,7 +10,6 @@ def insert_authors(loc):
 
 	f = open(loc,"r")
 	line = f.readline()
-	print "Starting....."
 	count =0
 	while line:
 		pro = json.loads(line)
@@ -38,5 +37,82 @@ def insert_authors(loc):
 
 	f.close()
 
-l = "/Users/pranayag/mtp/visualize/data/authors.txt"	
-insert_authors(l)
+def insert_followers(loc):
+	graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
+	batch = neo4j.WriteBatch(graph_db)
+	BATCH_SIZE = 400
+
+	f = open(loc,"r")
+	line = f.readline()
+	count =0
+	while line:
+		entry = json.loads(line)
+		uid = entry['author']
+		author = graph_db.get_indexed_node("users", "uid", str(uid))
+		if(author):
+			followers = entry['followers']
+			for fid in followers:
+				batch.get_or_create_indexed_node("users","uid", fid ,{"uid" : fid,"is_author": "0"})
+			nodes = batch.submit()
+			# Create the relations
+			for n in nodes:
+				batch.get_or_create_relationship(n, "follows", author)
+			rels = batch.submit()
+			# print rels
+			print "A batch of relations inserted"
+		
+		else:
+			print "author not in db"
+		# print author
+		# print graph_db.get_properties(author)
+		# break
+		line = f.readline()
+
+	f.close()
+
+def insert_friends(loc):
+	graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
+	batch = neo4j.WriteBatch(graph_db)
+	BATCH_SIZE = 400
+
+	f = open(loc,"r")
+	line = f.readline()
+	count =0
+	while line:
+		entry = json.loads(line)
+		uid = entry['author']
+		# pprint(entry)
+		# break
+		author = graph_db.get_indexed_node("users", "uid", str(uid))
+		if(author):
+			friends = entry['friends']
+			for fid in friends:
+				batch.get_or_create_indexed_node("users","uid", fid ,{"uid" : fid,"is_author": "0"})
+			nodes = batch.submit()
+			
+			# Create the relations
+			for n in nodes:
+				batch.get_or_create_relationship(author, "follows", n)
+			
+			rels = batch.submit()
+			count+=1
+			# print rels
+			print "inserted batch id ",count
+		
+		else:
+			print "author not in db"
+		# print author
+		# print graph_db.get_properties(author)
+		# break
+		line = f.readline()
+	f.close()
+
+
+
+l1 = "/Users/pranayag/mtp/visualize/data/authors.txt"	
+l2 = "/Users/pranayag/mtp/visualize/data/followers.txt.head"	
+l3 = "/Users/pranayag/mtp/visualize/data/friends.txt"	
+
+# insert_authors(l1)
+# insert_followers(l2)
+insert_friends(l3)
