ee20b20e44dee6cd120c8e9dfac36a85c4103628
Pranay <agarwalpranaya@gmail.com>
2013-05-01 17:44:46 +0530

    checking 800 celebs

--- check_celebs.py
+++ check_celebs.py
@@ -5,13 +5,24 @@ from pprint import pprint
 
 graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
 
-f = open('celebs.txt',"r")
-entry = json.loads(f.readline())
+f = open('celebs/top_profile.txt',"r")
+line = f.readline()
+count =0
 
-celebs =  json.loads(entry['users'])
-for celeb in celebs:
-	id_ = str(celeb["id"])
+while line:
+	entry = json.loads(line)
+	celebs =  json.loads(entry['users'])
+	
+	# Checking if celeb in database.
+	for celeb in celebs:
+		id_ = str(celeb["id"])
+		n = graph_db.get_indexed_node("users", "uid", id_)
+		if(n):
+			count+=1
+			print graph_db.get_properties(n)
 
-	n = graph_db.get_indexed_node("users", "uid", id_)
-	if(n):
-		print graph_db.get_properties(n)
+	line = f.readline()
+
+f.close()
+
+print count			
--- get_info.py
+++ get_info.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
-
+# import urllib2
+import os
 
 def get_1():
 	html_doc = """
@@ -36,18 +37,32 @@ def get_1():
 	# 		print line
 
 
-def get_2():
-	html_doc = open('p2.html',"r").read()
-	soup = BeautifulSoup(html_doc)
-	l = soup.find_all('td',{"class" : 'statcol_name'})
-	for elem in l :
-		s = elem.contents[1].contents[0]
-		print(s[1+s.find('('):s.find(")")])
+def get_celeb_list():
+	files = os.listdir('celebs/web')
+	# print(files)
+	fw = open('celebs/top1000.txt',"a")
+
+	for fil in files:
+		html_doc = open('celebs/web/'+fil,"r").read()
+		soup = BeautifulSoup(html_doc)
+		l = soup.find_all('td',{"class" : 'statcol_name'})
+		for elem in l :
+			s = elem.contents[1].contents[0]
+			uname = s[1+s.find('('):s.find(")")]
+			fw.write(uname+"\n")
 		# break
-		# print (elem.contents[1].contents[0])
 
-	# print(l)
-	# print(l.string)
-	# print(l.contents[1].contents)
+	fw.close()
+	
 
-get_2()
+def get_from_web():
+	for i in range(1,11):
+		url = "http://twitaholic.com/top"+str(i)+"00/followers/"
+		page = urllib2.urlopen(url).read()
+		f = open('celebs/web/top'+str(i)+"00.html","w")
+		f.write(page)
+		f.close()
+
+# get_2()
+# get_from_web()
+get_celeb_list()
